b215e60e23f64bc89f2fa27694f50d3f310ed227
ivanfonin <ivanfonin@yandex.ru>
2022-11-07 18:35:45 +0300

    refactor: Вынести PropTypes ингредиента в отдельную константу

--- src/components/burger-constructor/BurgerConstructor.jsx
+++ src/components/burger-constructor/BurgerConstructor.jsx
@@ -1,6 +1,6 @@
 import Price from '../price/Price';
 import { ConstructorElement, Button } from '@ya.praktikum/react-developer-burger-ui-components';
-import PropTypes from 'prop-types';
+import { ingredientPropTypes } from '../../utils/constants';
 
 import styles from './BurgerConstructor.module.css';
 
@@ -40,19 +40,7 @@ function BurgerConstructor({ ingredients }) {
 }
 
 BurgerConstructor.propTypes = {
-  ingredients: PropTypes.arrayOf(PropTypes.shape({
-    _id: PropTypes.string,
-    name: PropTypes.string,
-    type: PropTypes.string,
-    proteins: PropTypes.number,
-    fat: PropTypes.number,
-    carbohydrates: PropTypes.number,
-    calories: PropTypes.number,
-    price: PropTypes.number,
-    image: PropTypes.string,
-    image_mobile: PropTypes.string,
-    image_large: PropTypes.string,
-  }))
+  ingredients: ingredientPropTypes
 }
 
 export default BurgerConstructor;
--- src/components/burger-ingredients/BurgerIngredients.jsx
+++ src/components/burger-ingredients/BurgerIngredients.jsx
@@ -1,7 +1,7 @@
 import Tabs from '../tabs/Tabs';
 import IngredientsSection from '../ingredients-section/IngredientsSection';
-import PropTypes from 'prop-types';
 import { groupBy } from '../../utils/helpers';
+import { ingredientPropTypes } from '../../utils/constants';
 
 import styles from './BurgerIngredients.module.css';
 
@@ -23,19 +23,7 @@ function BurgerIngredients({ ingredients }) {
 }
 
 BurgerIngredients.propTypes = {
-  ingredients: PropTypes.arrayOf(PropTypes.shape({
-    _id: PropTypes.string,
-    name: PropTypes.string,
-    type: PropTypes.string,
-    proteins: PropTypes.number,
-    fat: PropTypes.number,
-    carbohydrates: PropTypes.number,
-    calories: PropTypes.number,
-    price: PropTypes.number,
-    image: PropTypes.string,
-    image_mobile: PropTypes.string,
-    image_large: PropTypes.string,
-  }))
+  ingredients: ingredientPropTypes
 }
 
 export default BurgerIngredients;
