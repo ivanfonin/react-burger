5dd7e6c0c9962741252d9a1bb6c8e26dbe7348df
ivanfonin <ivanfonin@yandex.ru>
2022-11-27 13:50:11 +0300

    feat: Добавить Redux DevTools

--- src/store.jsx
+++ src/store.jsx
@@ -1,12 +1,12 @@
-import { createStore, applyMiddleware } from 'redux';
+import { compose, createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import rootReducer from './services/reducers';
 
-const sayHiMiddleWare = store => next => action => {
-  console.log(store.getState());
-  return next(action);
-};
+const composeEnhancers =
+  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    : compose;
 
-const enhancer = applyMiddleware(thunkMiddleware, sayHiMiddleWare);
+const enhancer = composeEnhancers(applyMiddleware(thunkMiddleware));
 
 export const store = createStore(rootReducer, enhancer);
