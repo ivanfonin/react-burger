5d0b4cd5ae162679f79a9a35201c09d1faa201a7
ivanfonin <ivanfonin@yandex.ru>
2023-05-23 14:11:14 +0300

    refactor: Создавать хранилище с помощью configureStore из @redux/toolkit

--- src/store.jsx
+++ src/store.jsx
@@ -1,12 +1,34 @@
-import { compose, createStore, applyMiddleware } from 'redux';
-import thunkMiddleware from 'redux-thunk';
-import rootReducer from './services/reducers';
+// import { compose, createStore, applyMiddleware } from 'redux';
+// import thunkMiddleware from 'redux-thunk';
+// import rootReducer from './services/reducers';
 
-const composeEnhancers =
-  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
-    : compose;
+// const composeEnhancers =
+//   typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+//     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+//     : compose;
 
-const enhancer = composeEnhancers(applyMiddleware(thunkMiddleware));
+// const enhancer = composeEnhancers(applyMiddleware(thunkMiddleware));
 
-export const store = createStore(rootReducer, enhancer);
+// export const store = createStore(rootReducer, enhancer);
+
+import { configureStore } from '@reduxjs/toolkit';
+
+import { burgerReducer } from './services/reducers/burger';
+import { checkoutReducer } from './services/reducers/checkout';
+import { ingredientReducer } from './services/reducers/ingredient';
+import { ingredientsReducer } from './services/reducers/ingredients';
+import { tabsReducer } from './services/reducers/tabs';
+import { userReducer } from './services/reducers/user';
+
+const store = configureStore({
+  reducer: {
+    burger: burgerReducer,
+    checkout: checkoutReducer,
+    ingredient: ingredientReducer,
+    ingredients: ingredientsReducer,
+    tab: tabsReducer,
+    user: userReducer,
+  },
+});
+
+export default store;
