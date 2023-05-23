5b918199f193ca5eaecd04130a2eec1cf0c77fae
ivanfonin <ivanfonin@yandex.ru>
2023-05-23 14:13:02 +0300

    refactor: Удалить старый код создания хранилища из файла

--- src/store.jsx
+++ src/store.jsx
@@ -1,16 +1,3 @@
-// import { compose, createStore, applyMiddleware } from 'redux';
-// import thunkMiddleware from 'redux-thunk';
-// import rootReducer from './services/reducers';
-
-// const composeEnhancers =
-//   typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-//     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
-//     : compose;
-
-// const enhancer = composeEnhancers(applyMiddleware(thunkMiddleware));
-
-// export const store = createStore(rootReducer, enhancer);
-
 import { configureStore } from '@reduxjs/toolkit';
 
 import { burgerReducer } from './services/reducers/burger';
