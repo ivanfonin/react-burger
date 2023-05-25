fff64fc5c54524d1be9b422f50153436008c7576
ivanfonin <ivanfonin@yandex.ru>
2023-05-25 17:33:41 +0300

    fix: Поправить хранилище после немного некорректного слияния веток из-за случайного ответвления от ветки month-9/step-1 и возвращения обратно

--- src/store.jsx
+++ src/store.jsx
@@ -1,21 +1,12 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { compose, createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import rootReducer from './services/reducers';
 
-import { burgerReducer } from './services/reducers/burger';
-import { checkoutReducer } from './services/reducers/checkout';
-import { ingredientReducer } from './services/reducers/ingredient';
-import { ingredientsReducer } from './services/reducers/ingredients';
-import { tabsReducer } from './services/reducers/tabs';
-import { userReducer } from './services/reducers/user';
+const composeEnhancers =
+  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    : compose;
 
-const store = configureStore({
-  reducer: {
-    burger: burgerReducer,
-    checkout: checkoutReducer,
-    ingredient: ingredientReducer,
-    ingredients: ingredientsReducer,
-    tab: tabsReducer,
-    user: userReducer,
-  },
-});
+const enhancer = composeEnhancers(applyMiddleware(thunkMiddleware));
 
-export default store;
+export const store = createStore(rootReducer, enhancer);
