aa3529abb3b8a0e9e1074359784926289c24c88e
ivanfonin <ivanfonin@yandex.ru>
2023-06-10 01:47:17 +0300

    refactor: Почистить предыдущие наработки

--- src/components/feed-orders/FeedOrder.jsx
+++
@@ -1,8 +0,0 @@
-import { useSelector } from 'react-redux';
-import { FeedOrder } from './feed-order/FeedOrder';
-
-export const FeedOrders = () => {
-  const { orders } = useSelector((state) => state.orders);
-
-  return orders.map((order) => <FeedOrder {...order}></FeedOrder>);
-};
