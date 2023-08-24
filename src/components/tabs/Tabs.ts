1647c178794346b76d2039f436ab55fe88be9f7b
Ivan Fonin <ivanfonin@yandex.ru>
2023-08-24 19:18:30 +0300

    refactor: Tabs.jsx > Tabs.ts

--- src/components/tabs/Tabs.ts
+++ src/components/tabs/Tabs.ts
@@ -5,18 +5,18 @@ import { useSelector } from 'react-redux';
 import styles from './Tabs.module.css';
 
 function Tabs({ onTabClick }) {
-  const active = useSelector(state => state.tab);
+  const active = useSelector((state) => state.tab);
 
   return (
     <nav className="pt-5">
       <ul className={styles.tabs}>
-        <Tab value="bun" active={ active === 'bun' } onClick={ onTabClick }>
+        <Tab value="bun" active={active === 'bun'} onClick={onTabClick}>
           Булки
         </Tab>
-        <Tab value="main" active={ active === 'main' } onClick={ onTabClick }>
+        <Tab value="main" active={active === 'main'} onClick={onTabClick}>
           Начинки
         </Tab>
-        <Tab value="sauce" active={ active === 'sauce' } onClick={ onTabClick }>
+        <Tab value="sauce" active={active === 'sauce'} onClick={onTabClick}>
           Соусы
         </Tab>
       </ul>
@@ -25,7 +25,7 @@ function Tabs({ onTabClick }) {
 }
 
 Tabs.propTypes = {
-  onTabClick: PropTypes.func.isRequired
-}
+  onTabClick: PropTypes.func.isRequired,
+};
 
 export default Tabs;
